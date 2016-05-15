95ed69c102430641dbe018205c5f2d936e076541
ДОМ <igor2870@qip.ru>
2016-05-15 22:33:23 +0300

    Добавлен Класс Exercise3
      цикл while

--- src/lesson/Exercise3.java
+++ src/lesson/Exercise3.java
@@ -4,4 +4,18 @@ package lesson;
  * Created by ДОМ on 15.05.2016.
  */
 public class Exercise3 {
+    public static void main(String [] args) {
+
+        int n = 10;
+
+        while (n > 0) {
+
+            System.out.println("tick " + n);
+
+            n--;
+
+        }
+
+    }
+
 }
