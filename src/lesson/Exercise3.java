95edb753c9c9077662e9f9ae083b859fb2821d6a
ДОМ <igor2870@qip.ru>
2016-05-15 22:41:56 +0300

    Добавлен Класс Exercise3
      цикл while
      реализован массив + сканер

--- src/lesson/Exercise3.java
+++ src/lesson/Exercise3.java
@@ -1,21 +1,48 @@
 package lesson;
-
+import java.util.Scanner;
 /**
  * Created by ДОМ on 15.05.2016.
  */
 public class Exercise3 {
     public static void main(String [] args) {
 
-        int n = 10;
+        int month_days[];
+int i;
+        month_days = new int[12];
 
-        while (n > 0) {
+        month_days[0] = 31;
 
-            System.out.println("tick " + n);
+        month_days[1] = 28;
 
-            n--;
+        month_days[2] = 31;
+
+        month_days[3] = 30;
+
+        month_days[4] = 31;
+
+        month_days[5] = 30;
+
+        month_days[6] = 31;
+
+        month_days[7] = 31;
+
+        month_days[8] = 30;
+
+        month_days[9] = 31;
+
+        month_days[10] = 30;
+
+        month_days[11] = 31;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input month namber");
+
+        i = sc.nextInt();
+        sc.close();
+
+        System.out.println("this month has  " + month_days[i] + " days.");
 
         }
 
     }
 
-}
+
