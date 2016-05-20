dce3039c03400dc03e9b8e8e9a57e87121d9a7d5
ДОМ <igor2870@qip.ru>
2016-05-20 23:21:01 +0300

    Добавлен Класс Exercise3
                                                                 цикл while
                                                                 реализован массив + сканер
                                                                 добавлен массив с названием месяцев
    
                                                                 в классе Test реализован подсчёт количества цифр в числе.
    
                                                                 в пакете lesson2 пока два класса для компИнфо
    
                                                                 добавлен Scanner для частоты;
                                                                   добавлен Scanner для памяти;

--- src/lesson2/ExForObjects.java
+++ src/lesson2/ExForObjects.java
@@ -8,14 +8,16 @@ import java.util.Scanner;
 public class ExForObjects {
     public static void main(String[] args) {
 double a;
+        int b;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Input F");
+        System.out.println("Input F"+" Input mem");
         a = sc.nextDouble();
+        b = sc.nextInt();
         sc.close();
         Ex1 ex1 = new Ex1();
         ex1.setName("ASUS");
         ex1.setfCpu(a);
-        ex1.setMem(12);
+        ex1.setMem(b);
         ex1.infoname();
         ex1.infoCpu();
         ex1.infoMem();
